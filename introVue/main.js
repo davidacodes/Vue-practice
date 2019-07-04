f117e74371043a1a8d630535b53381f4c480c067
davidalex620 <spand3r@gmail.com>
2019-07-04 15:34:51 -0500

    intial push

---
+++ introVue/main.js
@@ -0,0 +1,9 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        product: 'Socks',
+        image: './assets/vmSocks-green.jpg',
+        url: 'https://vuejs.org',
+        inStock: true
+    }
+})
