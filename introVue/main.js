dc607eff36938045ac314d8561a4b27531dab374
davidalex620 <spand3r@gmail.com>
2019-07-05 23:35:10 -0500

    worked further into course

--- introVue/main.js
+++ introVue/main.js
@@ -4,6 +4,34 @@ var app = new Vue({
         product: 'Socks',
         image: './assets/vmSocks-green.jpg',
         url: 'https://vuejs.org',
-        inStock: true
+        inventory: 100,
+        onSale: true,
+        details: ["80% cotton", "20% polyester", "Gender-neutral"],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: "green",
+                variantImage: "./assets/vmSocks-green.jpg"
+            },
+            {
+                variantId: 2235,
+                variantColor: "blue",
+                variantImage: "./assets/vmSocks-blue.jpg"
+            }
+        ],
+        cart: 0
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        updateProduct(variantImage) {
+            this.image = variantImage
+        },
+        removeFromCart() {
+            if (this.cart > 0) {
+                this.cart -= 1
+            }
+        }
     }
 })
